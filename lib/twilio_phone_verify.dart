0844c4ee276375b02ee333e1d3236046499257f2
aliumairdev <aliumair.dev@gmail.com>
2023-09-22 17:02:42 +0500

    init with updated Http

---
+++ lib/twilio_phone_verify.dart
@@ -0,0 +1,14 @@
+// You have generated a new plugin project without specifying the `--platforms`
+// flag. A plugin project with no platform support was generated. To add a
+// platform, run `flutter create -t plugin --platforms <platforms> .` under the
+// same directory. You can also find a detailed instruction on how to add
+// platforms in the `pubspec.yaml` at
+// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+
+library twilio_phone_verify;
+
+export 'src/api.dart';
+export 'src/model/verification.dart';
+export 'src/model/twilio_reponse.dart';
+export 'src/helpers.dart';
+export 'src/model/email_channel_configuration.dart';
---
+++ test/twilio_phone_verify_test copy.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:twilio_phone_verify/twilio_phone_verify.dart';
+
+void main() {
+
+}
